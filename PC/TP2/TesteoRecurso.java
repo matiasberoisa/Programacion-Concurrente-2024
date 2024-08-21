d403e8ae646818087a6b658354fc86c5bb21ab38
matias <mberoisa9@gmail.com>
2024-08-21 18:27:59 -0300

    acomodado

---
+++ PC/TP2/TesteoRecurso.java
@@ -0,0 +1,12 @@
+package PC.TP2;
+
+public class TesteoRecurso {
+    public static void main (String[] args){
+    Cliente juan=new Cliente();
+    juan.setName("Juan Lopez");
+    Cliente ines=new Cliente ();
+    ines.setName ("Ines Garcia");
+    juan.start();
+    ines.start();
+    }
+    }
