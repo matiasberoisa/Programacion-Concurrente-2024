7e8730b34b8c17bdc43d2fc1aaf8d433c1b0d86a
matias <mberoisa9@gmail.com>
2024-10-13 22:52:19 -0300

    TPO y tp6

--- PC/TP6/clases/Carril.java
+++ PC/TP6/clases/Carril.java
@@ -2,33 +2,41 @@ package TP6.clases;
 
 public class Carril {
     private String direccionActual;
-    private boolean listo = false;
+    private boolean listoNorte = true;
+    private boolean listoSur = false;
 
     public Carril() {
         direccionActual = "Norte";
     }
 
-    public synchronized void EntrarCocheDelNorte() throws InterruptedException {
-        while (!listo) {
+    @SuppressWarnings("unlikely-arg-type")
+    public synchronized void EntrarCocheDelNorte(Coche unCoche) throws InterruptedException {
+        while (!listoNorte && !unCoche.equals(direccionActual)) {
             wait(); // Espera hasta que otro hilo llame a notify
         }
-        System.out.println("Continuando...");
+        System.out.println("avanza por el norte el auto " + unCoche.getNumero());
+
     }
 
-    public synchronized void listoNorte() {
-        listo = true;
+    public synchronized void salirCocheDelNorte() {
+        listoSur = true;
+        listoNorte = false;
+        direccionActual = "Sur";
         notify(); // Notifica a los hilos en espera
     }
 
-    public synchronized void EntrarCocheDelSur() throws InterruptedException {
-        while (listo) {
+    @SuppressWarnings("unlikely-arg-type")
+    public synchronized void EntrarCocheDelSur(Coche unCoche) throws InterruptedException {
+        while (!listoSur && !unCoche.equals(direccionActual)) {
             wait(); // Espera hasta que otro hilo llame a notify
         }
-        System.out.println("Continuando...");
+        System.out.println("avanza por el sur el auto " + unCoche.getNumero());
     }
 
-    public synchronized void listoSur() {
-        listo = false;
+    public synchronized void salirCocheDelSur() {
+        listoNorte = true;
+        listoSur = false;
+        direccionActual = "Norte";
         notify(); // Notifica a los hilos en espera
     }
 
--- PC/TP6/clases/Coche.java
+++ PC/TP6/clases/Coche.java
@@ -12,6 +12,32 @@ public class Coche implements Runnable {
     }
 
     public void run() {
+        System.out.println("el coche n° " + this.numero + " se acerca al puente desde el " + this.direccion);
+        if (direccion.equals("Norte")) {
+            try {
+                puente.EntrarCocheDelNorte(this);
+                Thread.sleep(3000);
+                puente.salirCocheDelNorte();
 
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                puente.EntrarCocheDelSur(this);
+                Thread.sleep(3000);
+                puente.salirCocheDelSur();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getDireccion() {
+        return this.direccion;
+    }
+
+    public int getNumero() {
+        return this.numero;
     }
 }
